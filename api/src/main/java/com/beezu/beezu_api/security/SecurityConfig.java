e2af9f1b3b500575f09b4a0e4c8ffbf8dedb4247
ArthDroid1 <barbosarthur7@gmail.com>
2026-05-06 19:25:45 -0300

    chore(api): configure spring security and cors setup

---
+++ api/src/main/java/com/beezu/beezu_api/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.beezu.beezu_api.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    	
+        http
+            .csrf(csrf -> csrf.disable())
+            .cors(Customizer.withDefaults())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
+
+        return http.build();
+    }
+}
