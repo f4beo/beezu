d684bfa511b57c43091c5a94bca124e5be1cc5c3
ArthDroid1 <barbosarthur7@gmail.com>
2026-05-06 07:52:54 -0300

    chore(api): initial springboot config

---
+++ api/src/main/java/com/beezu/beezu_api/BeezuApiApplication.java
@@ -0,0 +1,13 @@
+package com.beezu.beezu_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeezuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeezuApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/beezu/beezu_api/BeezuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beezu.beezu_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeezuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
