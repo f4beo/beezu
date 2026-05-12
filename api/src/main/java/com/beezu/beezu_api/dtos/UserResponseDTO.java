fc71e4c9aa0eeb19a597e5f9374447f4a22a2bae
ArthDroid1 <barbosarthur7@gmail.com>
2026-05-11 22:03:14 -0300

    feat(api): add user repository and user DTOs

---
+++ api/src/main/java/com/beezu/beezu_api/dtos/UserResponseDTO.java
@@ -0,0 +1,5 @@
+package com.beezu.beezu_api.dtos;
+
+public record UserResponseDTO(Long id, String name, String email, Integer honey) {
+
+}
---
+++ api/src/main/java/com/beezu/beezu_api/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.beezu.beezu_api.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.beezu.beezu_api.models.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	Optional<User> findByEmail(String email);
+	boolean existsByEmail(String email);
+	List<User>findByName(String name);
+}
