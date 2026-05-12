29e9b6e38b69e25f9ef2e0ec59c00f90d629c28b
ArthDroid1 <barbosarthur7@gmail.com>
2026-05-12 13:12:29 -0300

    refactor(api): improve composition and domain modeling

--- api/src/main/java/com/beezu/beezu_api/models/Hive.java
+++ api/src/main/java/com/beezu/beezu_api/models/Hive.java
@@ -3,6 +3,7 @@ package com.beezu.beezu_api.models;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.Objects;
 @Entity
 @Table(name = "tb_hive")
 public class Hive implements Serializable {
@@ -10,16 +11,17 @@ public class Hive implements Serializable {
     @Id
     @Column(name = "discipline_id")
     private Long id;
-    @Column(name = "health")
+    @Column(name = "health", nullable = false)
     private Integer health;
-    @Column(name = "honey_level")
+    @Column(name = "honey_level", nullable = false)
     private Integer honeyLevel;
     @OneToOne
     @MapsId
-    @JoinColumn(name = "disciplie_id")
+    @JoinColumn(name = "discipline_id")
     private Discipline discipline;
 
-    public Hive(Integer health, Integer honeyLevel){
+    public Hive(Discipline discipline){
+    	this.discipline = discipline;
         this.health = 100;
         this.honeyLevel = 0;
     }
@@ -29,13 +31,13 @@ public class Hive implements Serializable {
     }
 
     public void addHoney(Integer amount){
-        if (amount == null || amount == 0) return;
+        if (amount == null || amount <= 0) return;
         this.honeyLevel += amount;
         updateHealth();
     }
 
     public void updateHealth(){
-        if(honeyLevel == 500){
+        if(honeyLevel >= 500){
             health = 100;
         } else if (honeyLevel >= 300) {
             health = 75;
@@ -70,4 +72,28 @@ public class Hive implements Serializable {
     public void setHoneyLevel(Integer honeyLevel) {
         this.honeyLevel = honeyLevel;
     }
+
+	public void setDiscipline(Discipline discipline) {
+		this.discipline = discipline;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hive other = (Hive) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+    
 }
