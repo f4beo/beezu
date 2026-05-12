297680a4efe4e68a650bef11f42f42a439b05fd6
ArthDroid1 <barbosarthur7@gmail.com>
2026-05-12 09:23:39 -0300

    refactor(api): add first repository methods

--- api/src/main/java/com/beezu/beezu_api/repositories/ActivityRepository.java
+++ api/src/main/java/com/beezu/beezu_api/repositories/ActivityRepository.java
@@ -1,9 +1,15 @@
 package com.beezu.beezu_api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.beezu.beezu_api.models.Activity;
+import com.beezu.beezu_api.models.enums.ActivityType;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
+	
+	List<Activity> findByDisciplineId(Long disciplineID);
+	List<Activity> findByActiviyType(ActivityType type);
 
 }
