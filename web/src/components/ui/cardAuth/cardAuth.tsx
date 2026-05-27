b65627751399b1179ecebde11d6e30b634f3bbd7
Petrux~ <a0812petrus@gmail.com>
2026-05-27 19:32:01 -0300

    feat(web): login page layout

---
+++ web/src/components/ui/cardAuth/cardAuth.tsx
@@ -0,0 +1,23 @@
+
+import styles from './cardAuth.module.css';
+import Image from 'next/image';
+import imagemLogo from '../../../../public/beezu-logo-secondary.svg';
+import { Children } from 'react';
+
+type CardAuthProps = {
+    children: React.ReactNode
+}
+
+export default function CardAuth({ children }: CardAuthProps) {
+    return (
+        <div className={styles.container}>
+       '     <div className={styles.cardAuth}>
+                <Image src={imagemLogo} alt="Logo da Beezu" className='logoBeezu'/>
+                { children }
+'
+            </div>
+
+            
+        </div>
+    )
+}
