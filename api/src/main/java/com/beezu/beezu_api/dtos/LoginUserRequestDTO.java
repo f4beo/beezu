f9fbebe74f0439edf714f91bc274827c2128db52
Fábio Amaral <leonardofafj@gmail.com>
2026-05-28 09:16:57 -0300

    fix(api): add email field validation

--- api/src/main/java/com/beezu/beezu_api/dtos/LoginUserRequestDTO.java
+++ api/src/main/java/com/beezu/beezu_api/dtos/LoginUserRequestDTO.java
@@ -1,7 +1,8 @@
 package com.beezu.beezu_api.dtos;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Email;
 
-public record LoginUserRequestDTO(@NotEmpty(message = "email is obrigatory") String email, @NotEmpty(message = "password is obrigatory")String password) {
+public record LoginUserRequestDTO(@NotEmpty(message = "email is obrigatory") @Email String email, @NotEmpty(message = "password is obrigatory")String password) {
 
 }
--- api/src/main/java/com/beezu/beezu_api/dtos/RegisterUserRequestDTO.java
+++ api/src/main/java/com/beezu/beezu_api/dtos/RegisterUserRequestDTO.java
@@ -1,8 +1,9 @@
 package com.beezu.beezu_api.dtos;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Email;
 
 public record RegisterUserRequestDTO(@NotEmpty(message ="name is obrigatory") String name,
-		@NotEmpty(message ="email is obrigatory") String email,@NotEmpty(message ="password is obrigatory") String password) {
+		@NotEmpty(message ="email is obrigatory") @Email String email,@NotEmpty(message ="password is obrigatory") String password) {
 
 }
