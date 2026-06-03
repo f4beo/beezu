a61f6b799a8b548cfdaaac273971cb8ccd239c04
Fábio Amaral <leonardofafj@gmail.com>
2026-06-03 20:25:32 -0300

    feat(web): create axios instance w/ jwt injection and auth connection

---
+++ web/src/core/services/api.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
+
+export const api = axios.create({
+  baseURL: API_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get('token');
+
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      Cookies.remove('token');
+      // TODO: Handle Redirect
+    }
+    return Promise.reject(error);
+  },
+);
