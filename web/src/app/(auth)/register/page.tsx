b1b3975ce3da737449b576094f2764d9bd0c8bb3
Fábio Amaral <leonardofafj@gmail.com>
2026-06-04 02:11:54 -0300

    feat(web): implements register flow with backend

--- web/src/app/(auth)/register/page.tsx
+++ web/src/app/(auth)/register/page.tsx
@@ -7,19 +7,31 @@ import { useAuth } from '@/core/hooks/useAuth';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { UserLogin } from '@/types/types';
+import { authService } from '@/core/services/authService';
 
 export default function LoginPage() {
-  const [formData, setFormData] = useState({ email: '', password: '' });
-  const [errors, setErrors] = useState({ email: '', password: '', others: '' });
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const [errors, setErrors] = useState({
+    name: '',
+    email: '',
+    password: '',
+    others: '',
+  });
 
   const validateFields = () => {
-    console.trace('de onde fui chamada?');
-    let newErrors = { email: '', password: '', others: '' };
+    let newErrors = { name: '', email: '', password: '', others: '' };
     let isValid = true;
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    console.log(formData);
+    if (!formData.name.trim()) {
+      newErrors.name = 'O campo nome é obrigatório.';
+      isValid = false;
+    }
 
     if (!formData.email.trim()) {
       newErrors.email = 'O campo e-mail é obrigatório.';
@@ -39,22 +51,19 @@ export default function LoginPage() {
     return isValid;
   };
 
-  const { login, isLoading } = useAuth();
   const router = useRouter();
 
   const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setErrors({ email: '', password: '', others: '' });
+    setErrors({ name: '', email: '', password: '', others: '' });
     try {
-      console.log(errors);
       validateFields();
-      console.log(errors);
-      await login(formData);
-      router.push('/dashboard');
+      await authService.register(formData);
+      router.push('/login');
     } catch (err) {
       setErrors((prev) => ({
         ...prev,
-        others: 'Falha no login. Verifique suas credenciais.',
+        others: 'Falha no registro. Verifique suas credenciais.',
       }));
       console.log(err);
     }
@@ -73,6 +82,19 @@ export default function LoginPage() {
     <>
       <CardAuth>
         <form onSubmit={handleSubmit} className={styles.form}>
+          <div className={styles.inputGroup}>
+            <label>Nome:</label>
+            <input
+              className={styles.input}
+              name="name"
+              type="name"
+              onChange={handleChange}
+            />
+            {errors.name && (
+              <span className={styles.errorSpan}>{errors.name}</span>
+            )}
+          </div>
+
           <div className={styles.inputGroup}>
             <label>E-mail:</label>
             <input
@@ -107,7 +129,7 @@ export default function LoginPage() {
           )}
         </form>
         <span className={styles.authRedirect}>
-          Ainda não tens uma conta? <a href="/register"> Criar uma conta</a>
+          Já tem uma conta? <a href="/login">Entrar</a>
         </span>
       </CardAuth>
     </>
